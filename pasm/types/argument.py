0b665c4eac9bf3abc1bd0b9adf9401cf4356a9c9
Evan Pratten <ewpratten@gmail.com>
2020-04-16 10:36:57 -0400

    add assembling for instructions

--- pasm/types/argument.py
+++ pasm/types/argument.py
@@ -1,5 +1,6 @@
 from typing import List
 from ..errors import addWarning, addError
+from dataclasses import dataclass
 
 @dataclass
 class Argument:
--- pasm/types/instruction.py
+++ pasm/types/instruction.py
@@ -3,11 +3,28 @@
 from dataclasses import dataclass
 from typing import List
 from .argument import Argument
+from ..errors import addWarning, addError
+
 
 @dataclass
-class Instruction: 
-    
-    mnemonic:str
-    binary:int
-    expected_args:int
-    args:List[Argument]
+class Instruction:
+
+    mnemonic: str
+    binary: int
+    expected_args: int
+    args: List[Argument]
+
+    def assemble(self) -> List[int]:
+
+        # Handle errors
+        if len(self.args != self.expected_args):
+            addError(
+                f"Instruction: {self.mnemonic} ! Expected {self.expected_args} args, got {len(self.args)}")
+
+        output = []
+
+        output.append(self.binary)
+        for arg in self.args:
+            output += arg.assemble()
+
+        return output
